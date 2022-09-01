12b7b77dae22449d6b694aba6947f417e704d199
nirobxhasan <thenirobhasan@gmail.com>
2022-09-01 14:58:36 +0600

    user model created

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ require('dotenv').config()
 
 import config from 'config'
 import express from 'express'
+import router from './routers'
 import connectDb from './utils/connectToDb'
 import log from './utils/logger'
 
 const app = express()
 const port = config.get("port")
 
+
+app.use(router)
 
 
 app.get('/', (req,res)=>{
